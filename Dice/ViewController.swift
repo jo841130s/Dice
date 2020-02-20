db62b5aa64c4fa6d6c4c0395c258d4e5b233e26a
黃冠中 <jo@jode-MacBook-Air.local>
2020-02-20 12:27:18 +0800

    Add Adbert Ad, change to 5 dice

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -13,11 +13,20 @@ import AVKit
 class ViewController: UIViewController, AdbertADBannerDelegate {
     
     var timer : Timer?
+    
+    @IBOutlet weak var diceView1: UIView!
+    @IBOutlet weak var diceView2: UIView!
+    @IBOutlet weak var diceView3: UIView!
+    @IBOutlet weak var diceView4: UIView!
+    @IBOutlet weak var diceView5: UIView!
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    @IBOutlet weak var dice3: UIImageView!
+    @IBOutlet weak var dice4: UIImageView!
+    @IBOutlet weak var dice5: UIImageView!
     @IBOutlet weak var centerView: UIView!
-    @IBOutlet weak var diceView: UIView!
-    @IBOutlet weak var diceImageView: UIImageView!
     @IBOutlet weak var upLabel: UILabel!
-    @IBOutlet weak var doenLabel: UILabel!
+    @IBOutlet weak var bannerView: UIView!
     var blackView = UIView()
     var banner : AdbertADBanner!
     
@@ -26,48 +35,75 @@ class ViewController: UIViewController, AdbertADBannerDelegate {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        diceView.layer.borderWidth = 10
-        diceView.layer.borderColor = UIColor.black.cgColor
-        diceView.layer.cornerRadius = 20
+        diceView1.layer.borderWidth = 10
+        diceView1.layer.borderColor = UIColor.black.cgColor
+        diceView1.layer.cornerRadius = 20
+        diceView1.isHidden = true
+        diceView2.layer.borderWidth = 10
+        diceView2.layer.borderColor = UIColor.black.cgColor
+        diceView2.layer.cornerRadius = 20
+        diceView2.isHidden = true
+        diceView3.layer.borderWidth = 10
+        diceView3.layer.borderColor = UIColor.black.cgColor
+        diceView3.layer.cornerRadius = 20
+        diceView4.layer.borderWidth = 10
+        diceView4.layer.borderColor = UIColor.black.cgColor
+        diceView4.layer.cornerRadius = 20
+        diceView4.isHidden = true
+        diceView5.layer.borderWidth = 10
+        diceView5.layer.borderColor = UIColor.black.cgColor
+        diceView5.layer.cornerRadius = 20
+        diceView5.isHidden = true
         view.backgroundColor = UIColor.gray
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        changeBackgroundColor()
-        diceImageView.image = UIImage(named: "1")
-        diceImageView.clipsToBounds = true
-        diceImageView.layer.cornerRadius = 20
+        view.backgroundColor = getRandomColor()
+        dice1.clipsToBounds = true
+        dice1.layer.cornerRadius = 20
+        dice2.clipsToBounds = true
+        dice2.layer.cornerRadius = 20
+        dice3.clipsToBounds = true
+        dice3.layer.cornerRadius = 20
+        dice4.clipsToBounds = true
+        dice4.layer.cornerRadius = 20
+        dice5.clipsToBounds = true
+        dice5.layer.cornerRadius = 20
         banner = AdbertADBanner(appid: "ad-adb-18e46e12d521", andAPPKEY: "a961268138235")
         banner.delegate = self
         banner.fullScreenBanner = false
         banner.adSize = CGSize(width: 320, height: 50)
-        banner.frame = CGRect(x:0, y:0, width:320, height:50)
+        banner.frame = CGRect(x:(UIScreen.main.bounds.width - banner.adSize.width)/2, y:0, width:320, height:50)
         banner.requestAD()
     }
     
-    func changeBackgroundColor() {
-        timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: (#selector(getRandomColor)), userInfo: nil, repeats: true)
-    }
     
-    @objc func getRandomColor() {
+    
+    func getRandomColor() -> UIColor {
         let red   = CGFloat((arc4random() % 256)) / 255.0
         let green = CGFloat((arc4random() % 256)) / 255.0
         let blue  = CGFloat((arc4random() % 256)) / 255.0
         let alpha = CGFloat(0.3)
-
-        UIView.animate(withDuration: 5.0, delay: 0.0, options:[.repeat, .autoreverse], animations: {
-            self.view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: alpha)
-        }, completion:nil)
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
     }
     
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
             AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
             rollingAnimation()
-            let number = Int.random(in: 1 ..< 7)
-            diceLayout(number: number)
+            let number1 = Int.random(in: 1 ..< 7)
+            let number2 = Int.random(in: 1 ..< 7)
+            let number3 = Int.random(in: 1 ..< 7)
+            let number4 = Int.random(in: 1 ..< 7)
+            let number5 = Int.random(in: 1 ..< 7)
+            diceLayout(number: number1, dice: dice1)
+            diceLayout(number: number2, dice: dice2)
+            diceLayout(number: number3, dice: dice3)
+            diceLayout(number: number4, dice: dice4)
+            diceLayout(number: number5, dice: dice5)
+            view.backgroundColor = getRandomColor()
         }
     }
     
@@ -94,21 +130,57 @@ class ViewController: UIViewController, AdbertADBannerDelegate {
         view.viewWithTag(123)?.removeFromSuperview()
     }
     
-    func diceLayout(number:Int) {
-        diceImageView.image = UIImage(named:"\(number)")
+    func diceLayout(number:Int,dice:UIImageView) {
+        dice.image = UIImage(named:"\(number)")
     }
     
     func adbertADViewDidReceiveAd(_ banner: AdbertADBanner!) {
-         print("receive")
+        print("receive")
+        bannerView.addSubview(banner)
+        banner.showAD()
+        
      }
     
      func adbertADView(_ banner: AdbertADBanner!, didFailToReceiveAdWithError error: Error!) {
-         print("Adbert Ad did fail to receive")
+        print("Adbert Ad did fail to receive")
      }
     
      func adbertADViewDidClicked(_ banner: AdbertADBanner!) {
-         print("Adbert Ad did clicked")
+        print("Adbert Ad did clicked")
      }
     
+    @IBAction func diceNumberControl(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            diceView1.isHidden = true
+            diceView2.isHidden = true
+            diceView3.isHidden = false
+            diceView4.isHidden = true
+            diceView5.isHidden = true
+        } else if sender.selectedSegmentIndex == 1 {
+            diceView1.isHidden = false
+            diceView2.isHidden = true
+            diceView3.isHidden = true
+            diceView4.isHidden = true
+            diceView5.isHidden = false
+        } else if sender.selectedSegmentIndex == 2 {
+            diceView1.isHidden = false
+            diceView2.isHidden = true
+            diceView3.isHidden = false
+            diceView4.isHidden = true
+            diceView5.isHidden = false
+        } else if sender.selectedSegmentIndex == 3 {
+            diceView1.isHidden = false
+            diceView2.isHidden = false
+            diceView3.isHidden = true
+            diceView4.isHidden = false
+            diceView5.isHidden = false
+        } else if sender.selectedSegmentIndex == 4 {
+            diceView1.isHidden = false
+            diceView2.isHidden = false
+            diceView3.isHidden = false
+            diceView4.isHidden = false
+            diceView5.isHidden = false
+        }
+    }
 }
 
